f3fedd13a15d017faa6ee60a9b8b9e9ff4d6b38b
Russell McClellan <russell.mcclellan@gmail.com>
2023-04-09 23:29:39 -0400

    search!

--- src/snippet.ts
+++ src/snippet.ts
@@ -14,7 +14,7 @@ export const load = async (): Promise<Env> => {
 
 export type Index = {
     model: use.UniversalSentenceEncoder,
-    snippets: [string][]
+    snippets: [string, tf.Tensor2D][]
 }
 
 export type IndexOptions = {
@@ -22,24 +22,44 @@ export type IndexOptions = {
     snippetLength?: number
 };
 
-const snippets = function* (document: string, snippetLength: number) {
-    const words = document.split(/(\s+)/);
+const extractSnippets = function* (document: string, snippetLength: number) {
+    const words = document.split(/[\s\n]+/m);
     for (let i = 0; i < words.length; i += snippetLength) {
         yield words.slice(i, i + snippetLength).join(" ");
     }
 }
 
-const mapped = function* <T, U>(f: (t: T) => U, i: Iterable<T>) {
-    for (const t of i) {
-        yield f(t)
+export const index = async (env: Env, document: string, options?: IndexOptions): Promise<Index> => {
+    const snippetLength = options?.snippetLength ?? 80;
+    const snippets = Array.from(extractSnippets(document, snippetLength));
+    const embedTensor = await env.model.embed(snippets);
+    const embeds = [...Array(snippets.length).keys()].map((i): [string, tf.Tensor2D] => {
+        return [snippets[i], tf.slice(embedTensor, [i, 0], [1])]
+    })
+    return {
+        model: env.model,
+        snippets: embeds,
     }
 }
 
-export const index = async (env: Env, document: string, options?: IndexOptions): Promise<Index> => {
-    const snippetLength = options?.snippetLength ?? 150;
+export type SearchOptions = {
+    numResults?: number
+}
 
-    return {
-        model: env.model,
-        snippets: Array.from(mapped((x) => [x], snippets(document, snippetLength)))
+export const search = async (index: Index, query: string, options?: SearchOptions): Promise<string[]> => {
+    const numResults = options?.numResults ?? 5;
+    const embedTensor = await index.model.embed(query);
+    let ret: [string, number][] = [];
+    for (const [snippet, embed] of index.snippets) {
+        const score = await tf.matMul(embedTensor, embed, false, true).data();
+        if (ret.length < numResults) {
+            ret.push([snippet, score[0]]);
+            ret.sort(([_a, n], [_b, m]) => n - m)
+        } else if (score[0] > ret[ret.length - 1][1]) {
+            ret.pop();
+            ret.push([snippet, score[0]]);
+            ret.sort(([_a, n], [_b, m]) => m - n)
+        }
     }
+    return ret.map(([v]) => v);
 }
